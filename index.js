de296170e4c7031577ace7ae27026bc0bbe1cfea
NydiaGal <135789457+NydiaGal@users.noreply.github.com>
2023-10-31 21:34:39 -0500

    final code

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-import fs from 'fs';
-import inquirer from 'inquirer';
-import { createSVG } from './lib/shapes.js';
+const fs = require('./fs');
+const inquirer = require("inquirer");
+const {Circle, Square, Triangle} = require("./lib/shapes");
 
-inquirer
-  .prompt([
+class Svg{
+  constructor(){
+      this.textElement = ''
+      this.shapeElement = ''
+  }
+  render(){
+
+      return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+  }
+  setTextElement(text,color){
+      this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+  }
+  setShapeElement(shape){
+      this.shapeElement = shape.render()
+
+  }
+}
+
+const questions = [
     {
       type: 'input',
       name: 'text',
@@ -25,7 +42,7 @@ inquirer
       name: 'shapeColor',
       message: 'Enter shape color:',
     },
-  ])
+  ]
   .then((input) => {
     const svg = generateSVGLogo(input);
     fs.writeFileSync('logo.svg', svg);
